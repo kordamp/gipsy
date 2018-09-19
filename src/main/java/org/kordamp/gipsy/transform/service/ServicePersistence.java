1dfb86dfa537e7faecd634a48743bcdf49451d54
aalmiray <aalmiray@gmail.com>
2018-09-19 22:48:18 +0200

    Updates for 0.5.0

--- src/main/java/org/kordamp/gipsy/transform/service/ServicePersistence.java
+++ src/main/java/org/kordamp/gipsy/transform/service/ServicePersistence.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.kordamp.gipsy.transform.service;
 
 import org.kordamp.gipsy.transform.AbstractFilePersistence;
